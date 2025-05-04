372dd8e829c36452cfaed9977097c1b25304d18e
Lucescu-Andrei <167630596+Lucescu-Andrei@users.noreply.github.com>
2025-05-05 00:01:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,16 @@ optiunile.forEach(varianta => {
     varianta.addEventListener('dragstart', (e) => {
         e.dataTransfer.setData('text/plain', e.target.id);
     });
-});
+  });
+
+  // varianta.classList.add("optiune-animatie"); Animația la opțiuni
+  // function animatiaOptiune()
+  // {
+  //   optiunile.forEach(varianta =>{
+  //     varianta.classList.add("optiune-animatie");
+
+  //   });
+  // }
 
 dropLocuri.forEach(dropLoc => {
     dropLoc.addEventListener('dragover', (e) => {
@@ -133,3 +142,49 @@ function afPunctaj()
 }
 
 /*AUTO-SCROLLER-UL*/
+
+/*BACKGROUND ANIMATION */
+// const icons = ["&#xf6e6", "&#xea0b", "&#xeb43"]; // Example icons. Use correct codes.
+// const numIcons = 20;
+// const backgroundIconsEffect = document.querySelector('.background-icons-effect');
+
+// for (let i = 0; i < numIcons; i++) {
+//     const span = document.createElement('span');
+//     span.classList.add('material-symbols-rounded');
+//     span.textContent = String.fromCodePoint(parseInt(icons[i % icons.length], 16)); // Correct way to add icons
+//     span.style.left = `${Math.random() * 100}vw`;
+//     span.style.top = `${Math.random() * 100}vh`;
+//     span.style.animationDelay = `${Math.random() * 8}s`;
+//     backgroundIconsEffect.appendChild(span);
+// }
+
+
+//Functi pentru denumirea muschiilor din imagine
+function afDenumireMuschi(event, text) {
+  let tooltip = document.getElementById("denumire-muschi");
+  tooltip.style.display = "block";
+  tooltip.textContent = text;
+  tooltip.style.left = event.pageX + 10 + "px";
+  tooltip.style.top = event.pageY + 10 + "px";
+}
+
+function hideDenumireMuschi() {
+  document.getElementById("denumire-muschi").style.display = "none";
+}
+
+//functie pentru schimbarea imaginii
+function schimbaImg() {
+  let img1 = document.getElementById("corp-uman-frontal");
+  let img2 = document.getElementById("corp-uman-spate");
+  if(img1.style.display == 'block')
+    {
+      img1.style.display ='none';
+      img2.style.display ='block';
+    }
+  else
+    {
+      img2.style.display ='none';
+      img1.style.display ='block';
+    }  
+
+} 
